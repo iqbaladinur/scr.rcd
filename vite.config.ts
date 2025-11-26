5dc2d9dfef9b02d3deebcc0ca8b0aa08ca51a4ab
iqbaladinur <iqbaladinur@gmail.com>
2025-11-26 19:05:05 +0700

    dev; change port dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,30 @@
-import path from "path"
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-import tailwind from "tailwindcss"
-import autoprefixer from "autoprefixer"
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
+    css: {
+        postcss: {
+            plugins: [tailwind(), autoprefixer()],
+        },
     },
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
-  optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
-  },
-  server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp'
-    }
-  }
-})
+    optimizeDeps: {
+        exclude: ["@ffmpeg/ffmpeg", "@ffmpeg/util"],
+    },
+    server: {
+        headers: {
+            "Cross-Origin-Opener-Policy": "same-origin",
+            "Cross-Origin-Embedder-Policy": "require-corp",
+        },
+        port: 1232,
+    },
+});
